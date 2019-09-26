e155d004c78c8bf57efdf853b9617fa59db87f4b
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:04:05 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan7/integer/PBOIF210118056Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * angka
+ */
+public class PBOIF210118056Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32764;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int :" + i);
+        System.out.println("long : " + 1);
+        
+    }
+    
+}
